fb8cb252849d68bbc5b4580a4854f8b8cf51b063
praveentata <praveentata16@gmail.com>
2017-09-12 14:21:04 -0500

    Adding MultipleXMLS files to git

---
+++ src/test/java/firstPackage/ClassOne.java
@@ -0,0 +1,13 @@
+package firstPackage;
+
+import org.testng.annotations.Test;
+
+public class ClassOne {
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("Testing 1");
+	}
+
+}
---
+++ src/test/java/firstPackage/ClassTwo.java
@@ -0,0 +1,13 @@
+package firstPackage;
+
+import org.testng.annotations.Test;
+
+public class ClassTwo {
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("Testing 2");
+	}
+
+}
